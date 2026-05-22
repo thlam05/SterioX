b5441eb0c00f4e89b1a2487cfeaef297e0df2b77
hlam3004 <hlam.30042005@gmail.com>
2026-05-22 20:17:01 +0700

    feature(client): update button componet

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,14 +1,13 @@
 ﻿import type { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary' | 'danger';
+  variant?: 'primary' | 'secondary';
   label?: string;
 }
 
 const variantStyles = {
-  primary: 'bg-primary hover:bg-primary-dark text-dark',
-  secondary: 'bg-secondary hover:bg-secondary-dark text-white',
-  danger: 'bg-danger hover:bg-danger-dark text-white',
+  primary: 'bg-primary text-primary-foreground hover:bg-secondary hover:text-secondary-foreground',
+  secondary: 'bg-secondary text-secondary-foreground hover:bg-accent hover:text-accent-foregroun',
 };
 
 export function Button({
@@ -20,10 +19,16 @@ export function Button({
   ...props
 }: ButtonProps) {
 
-  const baseLayout = 'px-4 py-2 font-medium rounded transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2';
+  const baseLayout = `
+    inline-flex items-center justify-center
+    px-4 py-2 font-medium text-sm rounded-md 
+    cursor-pointer
+    transition-all duration-200 
+    active:scale-[0.98]
+    disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none
+  `.replace(/\s+/g, ' ').trim();
 
   const variantClass = variantStyles[variant] || variantStyles.primary;
-
   const finalClassName = `${baseLayout} ${variantClass} ${className}`.trim();
 
   return (
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Login() {
   return (
     <>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center mt-2">
         <Button label={"Hello world"} ></Button>
       </div>
     </>
