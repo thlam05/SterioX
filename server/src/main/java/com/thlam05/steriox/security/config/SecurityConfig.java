cc545c518c6c9e8a0effc166c693200e23eb4f3e
thlam05 <108942151+thlam05@users.noreply.github.com>
2026-06-08 17:38:20 +0700

    config(socket): update socket config

--- server/src/main/java/com/thlam05/steriox/security/config/SecurityConfig.java
+++ server/src/main/java/com/thlam05/steriox/security/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests(request -> request
                 .requestMatchers(PUBLIC_ENDPOINT).permitAll()
                 .requestMatchers(HttpMethod.GET, "/streams", "/streams/*").permitAll()
+                .requestMatchers("/ws/**", "/ws-chat/**").permitAll()
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated());
 
